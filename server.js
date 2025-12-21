7ad91351f737f339b548d1412c28a54d7d5c970f
Searooo <searo360@gmail.com>
2025-12-21 14:37:44 +0100

    new

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ import cors from "cors";
 import metaforgeRouter from "./routes.js";
 
 const app = express();
+const cors = require('cors');
 const PORT = 8080;
 
 // allow frontend to call your backend
 app.use(cors({
   origin: ['https://raiderdle.com', 'https://www.raiderdle.com', 'https://host-3yl.pages.dev'],
+  credentials: true, // Erlaubt Cookies/Auth-Header, falls du sie später brauchst
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true, // Erlaubt Cookies/Auth-Header, falls du sie später brauchst
   optionsSuccessStatus: 204
 }));
 
