0304edfaa54fb76878941891e55d707b6ff72a05
Searooo <searo360@gmail.com>
2025-12-22 22:19:42 +0100

    /

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = 8080;
 
 // allow frontend to call your backend
 app.use(cors({
-  origin: ['https://raiderdle.com', 'https://www.raiderdle.com', 'https://host-3yl.pages.dev', 'https://erndkmn.github.io/host'],
+  origin: ['https://raiderdle.com', 'https://www.raiderdle.com', 'https://host-3yl.pages.dev', 'https://erndkmn.github.io'],
   credentials: true, // Erlaubt Cookies/Auth-Header, falls du sie später brauchst
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
