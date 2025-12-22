7e9a5d6ad597d6e7de8e27c524b1f2148beef066
Searooo <searo360@gmail.com>
2025-12-22 19:19:07 +0100

    .

--- server.js
+++ server.js
@@ -14,13 +14,6 @@ app.use(cors({
   optionsSuccessStatus: 204
 }));
 
-// Serve static images with CORS headers
-app.use('/api/icons/image', express.static(path.join(__dirname, 'icons'), {
-  setHeaders: (res) => {
-    res.set('Access-Control-Allow-Origin', '*');
-  }
-}));
-
 // parse json request bodies
 app.use(express.json());
 
