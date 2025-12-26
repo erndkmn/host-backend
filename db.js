c84f98777effccb8d92b441a9678a47294bbe437
Searooo <searo360@gmail.com>
2025-12-26 01:39:28 +0100

    ?

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Pool } = pkg;
 
 export const pool = new Pool({
   host: 'localhost',
-  user: 'raider_user',
+  user: 'raiderdle_user',
   password: 'raiderdle6438',
   database: 'bugs',
   port: 5432,
